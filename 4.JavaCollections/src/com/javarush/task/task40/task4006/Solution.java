8dc270ee81e2949a4d93aa2f18193c160dc4fe84
Mariya <89948278+Masha1221@users.noreply.github.com>
2022-02-18 12:22:14 +0200

    Отправка Get-запроса через сокет

--- 4.JavaCollections/src/com/javarush/task/task40/task4006/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4006/Solution.java
@@ -1,8 +1,7 @@
 package com.javarush.task.task40.task4006;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.URL;
 
@@ -17,17 +16,30 @@ public class Solution {
 
     public static void getSite(URL url) {
         try {
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            Socket socket = new Socket( url.getHost(), url.getDefaultPort());
 
-            connection.setRequestMethod("GET");
 
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            OutputStream outputStream = socket.getOutputStream();
+            // getOutputStream - возвращает входной поток вывода
+            String request = "GET "+url.getFile()+" HTTP/1.1\r\n";
+            request +="Host: "+url.getHost()+"\r\n\r\n";
+
+            outputStream.write(request.getBytes());
+            outputStream.flush();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String responseLine;
+            //getInputStream - возвращает InputStream поток ввода
 
             while ((responseLine = bufferedReader.readLine()) != null) {
                 System.out.println(responseLine);
             }
             bufferedReader.close();
+            outputStream.close();
+
+
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
