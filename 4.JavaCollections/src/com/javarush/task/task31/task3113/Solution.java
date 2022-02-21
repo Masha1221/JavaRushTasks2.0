8a5febc5ae9df77159a26105ae5ba0f8942615b3
Mariya <89948278+Masha1221@users.noreply.github.com>
2022-02-21 17:01:41 +0200

    Считать путь к папке с консоли

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -0,0 +1,60 @@
+package com.javarush.task.task31.task3113;
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
+
+/* 
+Что внутри папки?
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String dirName = reader.readLine();
+
+        Path directory = Paths.get(dirName);
+
+        if (!Files.isDirectory(directory)) {
+            System.out.println(directory.toString() + " - не папка.");
+        } else {
+            AtomicInteger folderCounter = new AtomicInteger();
+            AtomicInteger fileCounter = new AtomicInteger();
+            AtomicLong sizeCounter = new AtomicLong();
+
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    if (!dir.equals(directory)) folderCounter.incrementAndGet();
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    fileCounter.incrementAndGet();
+                    sizeCounter.addAndGet(attrs.size());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            System.out.println("Параметры папки " + directory.toString() + ":");
+            System.out.println("Всего папок - " + folderCounter.get());
+            System.out.println("Всего файлов - " + fileCounter.get());
+            System.out.println("Общий размер - " + sizeCounter.get() + " байт");
+        }
+
+
+
+
+        }
+
+
+
+    }
+
