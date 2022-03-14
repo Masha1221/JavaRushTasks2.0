f8603971c65942ba74dfc6e62c3e1d70cb97012f
Mariya <89948278+Masha1221@users.noreply.github.com>
2022-03-14 14:00:14 +0200

    Заполнили класс Snake

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,4 +1,58 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snake {
+
+    private List<SnakeSection> sections;
+    private boolean isAlive;
+    private SnakeDirection direction;
+    private int x;
+    private int y;
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public void setSections(List<SnakeSection> sections) {
+        this.sections = sections;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public boolean isAlive(){
+        return isAlive;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    public Snake(int x, int y){
+      sections = new ArrayList<SnakeSection>();
+      isAlive = true;
+      sections.add(new SnakeSection(x,y));
+    }
+
+    public int getX() {
+        return sections.get(0).getX();
+    }
+
+
+    public int getY() {
+        return sections.get(0).getY();
+    }
+
+    public  void  move(){
+
+    }
+
+
 }
--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -1,4 +1,21 @@
 package com.javarush.task.task23.task2312;
 
 public class SnakeSection {
+
+    private int x;
+    private int y;
+
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public SnakeSection(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
