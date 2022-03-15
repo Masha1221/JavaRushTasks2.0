2775520294ce26e97081c01570903e5b0a75a1e6
Mariya <89948278+Masha1221@users.noreply.github.com>
2022-03-15 13:53:56 +0200

    Запрет переопределения

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2302/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2302/Solution.java
@@ -7,11 +7,11 @@ package com.javarush.task.task23.task2302;
 public class Solution {
 
     public static class Listener {
-        public void onMouseDown(int x, int y) {
+       final public void onMouseDown(int x, int y) {
             // Do something when the mouse down event occurs
         }
 
-        public void onMouseUp(int x, int y) {
+      final public void onMouseUp(int x, int y) {
             // Do something when the mouse up event occurs
         }
     }
