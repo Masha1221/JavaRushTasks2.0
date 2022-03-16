bc519a50b389e16868fa2bbf9ad7d258f44f0ea0
Mariya <89948278+Masha1221@users.noreply.github.com>
2022-03-16 16:23:51 +0200

    Определила путь файла

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1516/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task15.task1516;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Файл или директория
+*/
+
+public class Solution {
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String fileName1 = scanner.nextLine();
+
+            if (fileName1.isEmpty()) {
+                break;
+            }
+
+            if (Files.isRegularFile(Path.of(fileName1))) {
+                System.out.println(Path.of(fileName1) + THIS_IS_FILE);
+            } else if (Files.isDirectory(Path.of(fileName1))) {
+                System.out.println(Path.of(fileName1) + THIS_IS_DIR);
+            }
+            else break;
+        }
+
+
+    }
+}
+
+
